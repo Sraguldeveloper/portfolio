61e2a30c8a676797690363ebdcb9104f156a22f8
Sraguldeveloper <sragul1015@gmail.com>
2025-08-18 08:20:19 +0530

    link change

--- src/App.tsx
+++ src/App.tsx
@@ -217,7 +217,7 @@ function App() {
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-phone mr-3"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.63A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-1.18 2.19l-.7.69a19 19 0 0 0 6 6l.69-.7a2 2 0 0 1 2.19-1.18 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
               +91 73973 63765
             </a>
-            <a href="https://www.linkedin.com/in/ragulsethuraman" target="_blank" rel="noopener noreferrer" className="flex items-center text-white hover:text-blue-200 transition-colors duration-300 text-lg">
+            <a href="https://www.linkedin.com/in/ragul-sethuraman/" target="_blank" rel="noopener noreferrer" className="flex items-center text-white hover:text-blue-200 transition-colors duration-300 text-lg">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin mr-3"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect width="4" height="12" x="2" y="9" /><circle cx="4" cy="4" r="2" /></svg>
               LinkedIn
             </a>
